ceadc0c39d9ce2365afd3640f664191704414ac9
voxlol <voxlol@outlook.com>
2016-03-08 16:26:51 -0600

    Use DATABASE_URL env var to connect to DB

--- config/connections.js
+++ config/connections.js
@@ -42,10 +42,11 @@ module.exports.connections = {
   ***************************************************************************/
   someMysqlServer: {
     adapter: 'sails-mysql',
-    host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    user: 'YOUR_MYSQL_USER',
-    password: 'YOUR_MYSQL_PASSWORD',
-    database: 'YOUR_MYSQL_DB'
+    url: process.env.DATABASE_URL
+    // host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
+    // user: 'YOUR_MYSQL_USER',
+    // password: 'YOUR_MYSQL_PASSWORD',
+    // database: 'YOUR_MYSQL_DB'
   },
 
   /***************************************************************************
